272ff21a8bec21d097d42e28693340ae583709d2
gobo-larry <wrgamejuter@gmail.com>
2025-07-21 13:19:25 +0100

    style the active linnk in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,25 +1,39 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import Link from "next/link";
+import React from "react";
 import { FaBug } from "react-icons/fa";
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
 
 const NavBar = () => {
-    const links = [
-        {label:"Dashboard", href:"/"},
-        {label:"Issues", href:"/issues"}
-
-    ]
+  const currentpath = usePathname();
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href="/"><FaBug /></Link>
-        <ul className='flex space-x-6'>
-            {links.map(link=>
-                <li><Link className='text-zinc-500 hover:text-zinc-800 transition-colors' href={link.href}>{link.label}</Link></li>
-            )}
-            
-            
-        </ul>
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <FaBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li>
+            <Link
+              className={classNames({
+                "text-zinc-900": link.href === currentpath,
+                "text-zinc-500": link.href !== currentpath,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
