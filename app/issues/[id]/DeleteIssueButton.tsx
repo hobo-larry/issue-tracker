b0856f1973b58f968348b85528f97a09949169be
gobo-larry <wrgamejuter@gmail.com>
2025-08-06 17:23:31 +0100

    show confirmation dialog

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,12 +1,36 @@
-import { Button } from '@radix-ui/themes'
-import React from 'react'
+"use client";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 
-const DeleteIssueButton = ({issueId}: {issueId: number}) => {
+const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button color="red" size="3" radius='small'>
-Delete Issue
-    </Button>
-  )
-}
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red" size="3" radius="small">
+          Delete Issue
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content maxWidth="450px">
+        <AlertDialog.Title>Delete Issue</AlertDialog.Title>
+        <AlertDialog.Description size="2">
+          Are you sure you want to delete this issue ? This action is
+          irreversible
+        </AlertDialog.Description>
 
-export default DeleteIssueButton
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red">
+              Delete Issue
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
+};
+
+export default DeleteIssueButton;
