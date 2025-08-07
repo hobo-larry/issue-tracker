5f1c2ff432ac3de87542d815a85cda659ef0211a
gobo-larry <wrgamejuter@gmail.com>
2025-08-07 14:52:47 +0100

    remove duplicate skeletons

--- app/issues/[id]/EditIssueButton.tsx
+++ app/issues/[id]/EditIssueButton.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const EditIssueButton = ({issueId}:{issueId:number}) => {
   return (
     <Link
-      href={`/issues/${issueId}/edit`}
+      href={`/issues/edit/${issueId}`}
       className="inline-flex items-center px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
     >
       <Pencil2Icon className="mr-2" />
