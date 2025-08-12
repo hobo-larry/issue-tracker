4a10fcb6bff34386e5258b1826d25d86df24d903
gobo-larry <wrgamejuter@gmail.com>
2025-08-12 21:31:53 +0100

    assign an issue to a user but getting error on select.item valu="", will be back to it later

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -6,7 +6,11 @@ export const issueSchema = z.object({
 });
 export const patchIssueSchema = z.object({
   title: z.string().min(1, "Title is required.").max(255).optional(),
-  description: z.string().min(1, "Description is required.").max(65535),
+  description: z
+    .string()
+    .min(1, "Description is required.")
+    .max(65535)
+    .optional(),
   assignedToUserId: z
     .string()
     .min(1, "AssignedToUserId is required")
