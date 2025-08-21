bbb4b819f2e7eca87f74c09a6e9544e3f7ddf81f
gobo-larry <wrgamejuter@gmail.com>
2025-08-21 11:48:45 +0100

    refactor: extract issuetable

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,32 +1,15 @@
-import { Table } from "@radix-ui/themes";
-
 import prisma from "@/prisma/client";
-import NextLink from "next/link";
 
-import { IssueStatusBadge, Link } from "@/app/components/index";
-
-import IssueActions from "./IssueActions";
-import { Issue, Status } from "@prisma/client";
-import { ArrowDownIcon, ArrowUpIcon } from "@radix-ui/react-icons";
 import Pagination from "@/app/components/Pagination";
-type SearchParams = Promise<{
-  status: Status;
-  orderBy: keyof Issue;
-  direction?: "asc" | "desc";
-  page: string;
-}>;
+import { Issue, Status } from "@prisma/client";
+import IssueActions from "./IssueActions";
+import IssueTable, { columnNames, IssueQuery } from "./IssueTable";
 
 interface Props {
-  searchParams: SearchParams;
+  searchParams: IssueQuery;
 }
 
 export default async function IssuesPage(props: Props) {
-  const columns: { label: string; value: keyof Issue; className?: string }[] = [
-    { label: "Issue", value: "title" },
-    { label: "Status", value: "status", className: "hidden md:table-cell" },
-    { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
-  ];
-
   const searchParams = await props.searchParams;
   const statuses = Object.values(Status);
 
@@ -41,9 +24,7 @@ export default async function IssuesPage(props: Props) {
     ? (searchParams.direction as "asc" | "desc")
     : "desc";
 
-  const orderBy = columns
-    .map((column) => column.value)
-    .includes(searchParams.orderBy)
+  const orderBy = columnNames.includes(searchParams.orderBy)
     ? { [searchParams.orderBy]: direction }
     : undefined;
   const page = parseInt(searchParams.page) || 1;
@@ -61,60 +42,8 @@ export default async function IssuesPage(props: Props) {
   return (
     <div>
       <IssueActions />
-      <Table.Root variant="surface">
-        <Table.Header>
-          <Table.Row>
-            {columns.map((column) => (
-              <Table.ColumnHeaderCell
-                key={column.value}
-                className={column.className}
-              >
-                <NextLink
-                  href={{
-                    query: {
-                      ...searchParams,
-                      orderBy: column.value,
-                      direction:
-                        searchParams.orderBy === column.value
-                          ? searchParams.direction === "asc"
-                            ? "desc"
-                            : "asc"
-                          : "asc",
-                    },
-                  }}
-                >
-                  {column.label}
-                </NextLink>
-                {column.value === searchParams.orderBy ? (
-                  searchParams.direction === "asc" ? (
-                    <ArrowUpIcon className="inline" />
-                  ) : (
-                    <ArrowDownIcon className="inline" />
-                  )
-                ) : null}
-              </Table.ColumnHeaderCell>
-            ))}
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {issues.map((issue) => (
-            <Table.Row key={issue.id}>
-              <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
-                <div className="block md:hidden">
-                  <IssueStatusBadge status={issue.status} />
-                </div>
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status} />
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                {issue.createdAt.toDateString()}
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
+      <IssueTable searchParams={searchParams} issues={issues} />
+
       <Pagination
         pageSize={pageSize}
         currentPage={page}
@@ -124,4 +53,3 @@ export default async function IssuesPage(props: Props) {
   );
 }
 export const dynamic = "force-dynamic";
-
