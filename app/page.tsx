ebabb85d4b216fbc3775ebaa1dbe5003182dcd0f
gobo-larry <wrgamejuter@gmail.com>
2025-08-26 10:52:58 +0100

    build the lastest issue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,4 @@
-import Pagination from "./components/Pagination";
-
-
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
-  return (
-    <div>
-      <Pagination
-        itemCount={100}
-        pageSize={10}
-        currentPage={parseInt(searchParams.page)}
-      />
-    </div>
-  );
+import LatestIssues from "./LatestIssues";
+export default async function Home() {
+  return <LatestIssues />;
 }
