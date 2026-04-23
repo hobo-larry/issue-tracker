d32363a82e8a2a709fb2456ab0b2a775de8b6a32
hobo-larry <47593826+hobo-larry@users.noreply.github.com>
2026-04-23 11:06:25 +0100

    removed middleware.ts because it uses proxy.ts now

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-export { default } from "next-auth/middleware"
-
-export const config = {
-    matcher:[
-        '/issues/new',
-        '/issues/edit/:id+'
-        // will be adding more
-    ]
-}
