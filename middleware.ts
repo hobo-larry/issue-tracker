8fd31d30397d23165653db423b556216d20e666a
hobo-larry <47593826+hobo-larry@users.noreply.github.com>
2026-08-05 13:42:43 +0100

    removed everyting inside withauth to test the redirect issue

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,7 @@
 import { withAuth } from "next-auth/middleware";
 
 // O Next.js exige que o export seja "default" ou uma função chamada "middleware"
-export default withAuth({
-  pages: {
-    signIn: "/api/auth/signin",
-  },
-});
+export default withAuth();
 
 export const config = {
   // As rotas que você quer proteger
