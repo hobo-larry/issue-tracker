47b170112d1b6ed539d911d5d49aa343ffabf430
hobo-larry <47593826+hobo-larry@users.noreply.github.com>
2026-08-05 13:51:11 +0100

    testing middleware

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,6 @@
-// middleware.ts
-import { withAuth } from "next-auth/middleware";
-
-// O Next.js exige que o export seja "default" ou uma função chamada "middleware"
-export default withAuth();
+export { default } from "next-auth/middleware";
 
 export const config = {
   // As rotas que você quer proteger
-  matcher: [
-    "/issues/new",
-    "/issues/edit/:id+",
-  ],
+  matcher: ["/issues/new", "/issues/edit/:id+"],
 };
