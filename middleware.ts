1bbf514a296d978edd0cc688588878d7707109e4
hobo-larry <47593826+hobo-larry@users.noreply.github.com>
2026-08-06 11:23:47 +0100

    middleware update

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,10 @@
-export { default } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export function middleware() {
+  return NextResponse.next();
+}
 
 export const config = {
+  // As rotas que você quer proteger
   matcher: ["/issues/new", "/issues/edit/:path*", "/api/users", "/api/issues"],
 };
