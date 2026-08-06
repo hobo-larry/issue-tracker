dd769cb82f4e066cf8e5366e61d865c78ff0eb81
hobo-larry <47593826+hobo-larry@users.noreply.github.com>
2026-08-06 10:52:43 +0100

    testing middleware for api endpoints

--- middleware.ts
+++ middleware.ts
@@ -6,5 +6,5 @@ export function middleware() {
 
 export const config = {
   // As rotas que você quer proteger
-  matcher: ["/issues/new", "/issues/edit/:id+"],
+  matcher: ["/issues/new", "/issues/edit/:id+", "/api/:path*"],
 };
