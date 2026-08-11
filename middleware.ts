ed1e7209fb964f1abceb56121540741dcec32fd3
hobo-larry <47593826+hobo-larry@users.noreply.github.com>
2026-08-11 08:55:38 +0100

    testing middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,12 @@
-import { NextResponse } from "next/server";
+import { withAuth } from "next-auth/middleware";
 
-export function middleware() {
-  return NextResponse.next();
-}
+export default withAuth({
+  pages: {
+    signIn: "/api/auth/signin", // or your custom login page
+  },
+});
 
 export const config = {
   // As rotas que você quer proteger
-  matcher: ["/issues/new", "/issues/edit/:path*", "/api/users", "/api/issues"],
+  matcher: ["/issues/new", "/issues/edit/:path*", "/api/users"],
 };
